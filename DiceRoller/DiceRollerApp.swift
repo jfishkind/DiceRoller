c05bce03c3938534ea997fb1853fae8b6f1948b6
jfishkind <jake.fishkind@gmail.com>
2023-10-17 17:09:51 -0400

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Jake Fishkind on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
